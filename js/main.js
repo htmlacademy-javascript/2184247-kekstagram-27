5876180da20c30dca6fe88289cbd7f35c18ba9f8
AelitaSerour <114419462+AelitaSerour@users.noreply.github.com>
2022-10-09 18:04:05 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
-console.log(Math.floor(Math.random() * 100) + 1);
+console.log(Math.floor(Math.random() * 100) + 1); // Стащила с developer.mozilla из теории про Math-функции:)
+String.prototype.trimEllip = function (length) {
+  return this.length > length ? this.substring(0, length) + "..." : this;
+} // Нашла на codegrepper, но не уверена, что это то, что нужно
