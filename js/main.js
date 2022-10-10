b29833769921b3e72832d0feba0695a2f8ce905f
Aelita Serour <lecoledellie@gmail.com>
2022-10-10 18:07:41 +0200

    сomment:))

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-console.log(Math.floor(Math.random() * 100) + 1); // Стащила с developer.mozilla из теории про Math-функции:)
+function getRandomPositiveInteger (a, b) {
+  if (a < 0 || b < 0) {
+    return NaN;
+  }
 function checkStringLength (string, length) {
   return string.length <= length;
-} // Нашла на codegrepper, но не уверена, что это то, что нужно
+} 
