396a4b07a9cef9d55a1d6ef483c1b70fb94cb65a
AelitaSerour <114419462+AelitaSerour@users.noreply.github.com>
2022-10-13 18:31:09 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+const NAME = [
+  'Adam',
+  'Adele',
+  'Adrien',
+  'Agathe',
+  'Alan',
+  'Albert',
+  'Barnabe',
+  'Bastien',
+  'Benjamine',
+  'Bernadette',
+  'Berenice',
+  'Bernard',
+  'Blanche',
+  'Brigitte',
+  'Camille',
+  'Carole',
+  'Cassandra',
+  'Chantal',
+  'Charlotte',
+  'Claudette',
+  'Colette',
+  'Damien',
+  'Daphnee',
+  'Deborah',
+  'Desire',
+];
+
+
 const numberID = getNumber(ARRAY_NUMBER);
   return {
 id: getRandomInteger(1,25); 
