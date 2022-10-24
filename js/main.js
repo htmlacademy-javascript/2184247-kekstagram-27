7b38f171cdc8fdabd81923fc92f46cdf497ae991
AelitaSerour <114419462+AelitaSerour@users.noreply.github.com>
2022-10-24 15:27:48 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+import './util.js';
+import './data.js';
+import {generatePicture} from './data.js';
+generatePicture();
+
+
 function checkStringLength (string, length) {
   return string.length <= length;
 }
