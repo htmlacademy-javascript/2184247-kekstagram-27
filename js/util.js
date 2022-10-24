c25805aaa877f464480a2a58fe9965911a4f8674
AelitaSerour <114419462+AelitaSerour@users.noreply.github.com>
2022-10-24 15:33:21 +0200

    Create util.js

---
+++ js/util.js
@@ -0,0 +1,13 @@
+export const getRandomNumber = (min, max) => {
+  if (min > max) {
+    return NaN;
+  }
+  return Math.floor (Math.random() * (max - min + 1)) + min;
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
+checkStringLength (1, 300);
