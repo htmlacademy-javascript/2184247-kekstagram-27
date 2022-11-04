4a36b4307a0267091ffa7482b95ec8fb81eeb0a5
AelitaSerour <114419462+AelitaSerour@users.noreply.github.com>
2022-11-04 20:01:06 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
 import {generatePictures} from './data.js';
-<<<<<<< HEAD
-import './miniatures.js';
-import './full-size-pictures.js';
-import './users-form.js';
-import './upload.js';
-
-const PICTURES_COUNT = 25;
-
-generatePictures(PICTURES_COUNT);
-=======
 import {renderPictureMiniatures} './miniatures.js';
 
 const PICTURES_COUNT = 25;
@@ -16,4 +6,4 @@ const PICTURES_COUNT = 25;
 const pictures = generatePictures(PICTURES_COUNT);
 
 renderPictureMiniatures(pictures);
->>>>>>> master
+
